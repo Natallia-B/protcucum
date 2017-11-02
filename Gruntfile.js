51a0899fbe335219c56f37d27e329e2a830998d1
Natallia-B <nokatami@Natallias-MacBook-Pro.local>
2017-11-02 22:26:17 +0300

    last working grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,172 @@
-module.exports = function (grunt){
-grunt.initConfig({
-    // protractor_webdriver: {
-    //     options: {
-    //         path: 'node_modules/protractor/bin/',
-    //         keepAlive: true
-    //     },
-    //     update: {
-    //         options: {
-    //             //role: 'hub',
-    //             command: 'webdriver-manager update' // --role=hub, which will work if args are passed!
-    //         }
-    //     },
-    //     start: {
-    //         options: {
-    //             //role: 'hub',
-    //             command: 'webdriver-manager start' // --role=hub, which will work if args are passed!
-    //         }
-    //     }
-    // },
-
-    protractor: {
-        options: {
-            keepAlive: true, 
-            noColor: false 
-        },
-        dev: {
-            options: {
-                configFile: './conf.js',
-                args: {
-                    seleniumAddress:'http://localhost:4444/wd/hub',
-                    // baseUrl:'https://next.primary-intel.com/',
-                    cucumberOpts: {
-                        tags: ['~@ignore','<%= protractor.platformTag%>']
+module.exports = function(grunt) {
+    
+        var platform = grunt.option('platform'),
+            testHost = grunt.option('host'),
+            selenium = grunt.option('selenium'),
+            moment = require('moment');
+    
+        require('load-grunt-tasks')(grunt);
+    
+        grunt.initConfig({
+    
+            protractor_webdriver: {
+                options: {
+                    path: 'node_modules/protractor/bin/',
+                    keepAlive: true
+                },
+                update: {
+                    options: {
+                        //role: 'hub',
+                        command: 'webdriver-manager update' // --role=hub, which will work if args are passed!
+                    }
+                },
+                start: {
+                    options: {
+                        //role: 'hub',
+                        command: 'webdriver-manager start' // --role=hub, which will work if args are passed!
                     }
                 }
+            },
+    
+            protractor: {
+                options: {
+                    keepAlive: true, // If false, the grunt process stops when the test fails.
+                    noColor: false // If true, protractor will not use colors in its output.
+                },
+                dev: {
+                    options: {
+                        configFile: './conf.js',
+                        args: {
+                            seleniumAddress: selenium||process.env.SELENIUM_SERVER||'http://localhost:4444/wd/hub',
+                            baseUrl: testHost||process.env.HOST||'https://next.primary-intel.com/',
+                            // cucumberOpts: {
+                            //     tags: ['~@ignore','<%= protractor.platformTag%>']
+                            // }
+                        }
+                    }
+                },
+                // wip: {
+                //     options: {
+                //         configFile: 'test/e2e/protractor-conf.js',
+                //         args: {
+                //             seleniumAddress: selenium||process.env.SELENIUM_SERVER||'http://localhost:4444/wd/hub',
+                //             baseUrl: testHost||process.env.HOST||'https://www.travelsupermarket.com/',
+                //             cucumberOpts: {
+                //                 tags: ['@wip','<%= protractor.platformTag%>']
+                //             }
+                //         }
+                //     }
+                // },
+                // all: {
+                //     options: {
+                //         configFile: 'test/e2e/protractor-conf-multirun.js',
+                //         args: {
+                //             seleniumAddress: selenium||process.env.SELENIUM_SERVER||'http://localhost:4444/wd/hub',
+                //             baseUrl: testHost||process.env.HOST||'https://www.travelsupermarket.com/',
+                //             cucumberOpts: {
+                //                 tags: ['~@ignore','@desktop','@mobile','@tabletL','@tabletP','@journey']
+                //             }
+                //         }
+                //     }
+                // },
+            //     func: {
+            //         options: {
+            //             configFile: 'test/e2e/protractor-conf-multifunc.js',
+            //             args: {
+            //                 seleniumAddress: selenium||process.env.SELENIUM_SERVER||'http://localhost:4444/wd/hub',
+            //                 baseUrl: testHost||process.env.HOST||'https://www.travelsupermarket.com/',
+            //                 cucumberOpts: {
+            //                     tags: ['~@ignore','~@journey','<%= protractor.platformTag%>']
+            //                 }
+            //             }
+            //         }
+            //     }
+            },
+    
+            'protractor-cucumber-html-report': {
+                options: {
+                    dest: 'reports/gr',
+                    output: 'report.html',
+                    testJSONDirectory: 'reports/cucumber'
+                },
+                dev: {
+                    reportTitle: 'Test report generated by dev run'
+                },
+                // wip: {
+                //     reportTitle: 'Test report generated by wip run'
+                // },
+                // all: {
+                //     reportTitle: 'Test report generated by all run'
+                // },
+                // func: {
+                //     reportTitle: 'Test report generated by func run'
+                // }
+            },
+    
+        });
+    
+        grunt.loadNpmTasks('grunt-protractor-runner');
+        grunt.loadNpmTasks('grunt-protractor-webdriver');
+        grunt.loadNpmTasks('grunt-protractor-cucumber-html-report');
+    
+        grunt.registerTask('e2e', 'Run e2e tests', function(target) {
+            process.env.NODE_TLS_REJECT_UNAUTHORIZED=0;
+            process.env.START_TIME = moment().format('X');
+            var platformTag = '';
+    
+            if (platform){
+                process.env.PLATFORM=platform;
             }
-        }
-    }})
+            if(!process.env.PLATFORM){
+                process.env.PLATFORM='desktop';
+            }
+            if (selenium){
+                process.env.SELENIUM_SERVER=selenium;
+            }
+            if(!process.env.SELENIUM_SERVER){
+                process.env.SELENIUM_SERVER='http://localhost:4444/wd/hub';
+            }
+            if (testHost){
+                process.env.HOST=testHost;
+            }
+            if(!process.env.HOST){
+                process.env.HOST='https://primary-intel.com/';
+            }
     
-  
-    grunt.loadNpmTasks('grunt-protractor-runner');
-    grunt.loadNpmTasks('grunt-protractor-webdriver');
-    grunt.loadNpmTasks('grunt-protractor-cucumber-html-report');
+            if(process.env.PLATFORM !== 'mobile' && process.env.PLATFORM !== 'desktop' && process.env.PLATFORM !== 'tabletP' && process.env.PLATFORM !== 'tabletL'){
+                throw Error('Wrong type of platform: '+process.env.PLATFORM+'. Should be desktop, mobile, tabletP or tabletL.');
+            }
     
-    grunt.registerTask('e2e', 'Run e2e tests', function(target) {
-        console.log*("aaa");
-        // grunt.option('platform') ? 
-        //     process.env.PLATFORM = grunt.option('platform') :
-        //     process.env.PLATFORM = 'desktop';
-        // platformTag = '@'+ process.env.PLATFORM;
-        // grunt.log.writeflags(platformTag, 'Protractor');
-        // grunt.config.set('protractor.platformTag', platformTag);
-        target = target || 'dev';
-        grunt.task.run([/*'protractor_webdriver:start',*/
-            'protractor:'+target ,
-            /*'protractor-cucumber-html-report:'+target*/]);
-    });
-};
+            platformTag = '@'+ process.env.PLATFORM;
+    
+            grunt.log.writeflags(platformTag, 'Protractor');
+            grunt.config.set('protractor.platformTag', platformTag);
+    
+            console.log(' \nTest ENVIRONMENT: ');
+            console.log('    '+process.env.HOST);
+            console.log(' \nUsing Selenium Grid: ');
+            console.log('    '+process.env.SELENIUM_SERVER);
+            if(target !== 'all'){
+                console.log(' \nRun tests on platform: ');
+                console.log('    '+process.env.PLATFORM);
+            }
+            console.log(' \nTest start time: ');
+            console.log('    '+moment.unix(process.env.START_TIME).format('h:mm:ss a'));
+            console.log('\n');
+    
+            target = target || 'dev';
+    
+            process.env.RUN_TYPE = target;
+    
+            if(process.env.SELENIUM_SERVER === 'http://localhost:4444/wd/hub'){
+                grunt.task.run(['protractor_webdriver:start', 'protractor:'+target , 'protractor-cucumber-html-report:'+target]);
+            }else{
+                grunt.task.run(['protractor:'+target , 'protractor-cucumber-html-report:'+target]);
+            }
+        });
+    
+        grunt.registerTask('webdriver', 'webdriver items', function(target) {
+            grunt.task.run(['protractor_webdriver:'+target]);
+        });
+    
+    };
--- conf.js
+++ conf.js
@@ -1,45 +1,53 @@
 
 exports.config = {
-    framework: 'custom',
-    frameworkPath: require.resolve('protractor-cucumber-framework'), 
-    
-    cucumberOpts: {
-      // format:  'pretty',
-      require: [
-        'support/support/world.js',
-        'step_definitions/*.js'
-      ]
-    },
-  
-    
-  
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['features/*.feature'],
-    multiCapabilities: [{
-      browserName: 'chrome', 
 
-      // browserName: 'firefox',
-      // browsername: 'safari'
+  
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['features/first.feature'],
+  Capabilities: {
+    browserName: 'chrome', 
+
+    // browserName: 'firefox',
+    // browsername: 'safari'
+  },
+  allScriptsTimeout: 120000,
+
+  onPrepare: function(){
+    console.log('!!!!!!!!!!!!!!!!!!');
+    // browser.ignoreSynchronization = false;
+    // browser.driver.manage().window().maximize();
+    browser.driver.manage().window().setSize(1100, 800);
+    let chai = require('chai');
+    chai.use(require('chai-smoothie'));
+    
+    chaiAsPromised = require('chai-as-promised');
+    chai.use(chaiAsPromised);
+    expect = chai.expect;
+    function browserClearing() {
+      return Promise.resolve()
+        .then(() => browser.executeScript('window.sessionStorage.clear();'))
+        .then(() => browser.executeScript('window.localStorage.clear();'))
+        .then(() => browser.manage().deleteAllCookies());
     }
-  ],
-    allScriptsTimeout: 120000,
-
-    onPrepare: function(){
-      // browser.ignoreSynchronization = false;
-      // browser.driver.manage().window().maximize();
-      browser.driver.manage().window().setSize(1100, 800);
-      let chai = require('chai');
-      chai.use(require('chai-smoothie'));
-      
-      chaiAsPromised = require('chai-as-promised');
-      chai.use(chaiAsPromised);
-      expect = chai.expect;
-      function browserClearing() {
-        return Promise.resolve()
-          .then(() => browser.executeScript('window.sessionStorage.clear();'))
-          .then(() => browser.executeScript('window.localStorage.clear();'))
-          .then(() => browser.manage().deleteAllCookies());
-      }
-      }
+    },
+    // framework: 'custom',
+    // frameworkPath: require.resolve('protractor-cucumber-framework'), 
+    // cucumberOpts: {
+    //   // format:  'pretty',
+    //   require: [
+    //     'support/support/world.js',
+    //     'step_definitions/*.js'
+    //   ]
+    // },
+    framework: 'custom',
+    frameworkPath: require.resolve('protractor-cucumber-framework'),
+    cucumberOpts : {
+        require : [
+          'support/support/world.js',
+          'step_definitions/*.js'
+        ],
+        // format : 'rerun:test/e2e/reports/@rerun.txt'
+        // formats : ['pretty','rerun:@rerun.txt']
+    },
 };
-  
